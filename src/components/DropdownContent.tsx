801eba88328ebd94e44625418219a968d475a5ff
bjthe4 <bjthe4@gmail.com>
2025-04-29 15:21:50 -0500

    [CG1-11] Inital commits

--- src/components/DropdownContent.tsx
+++ src/components/DropdownContent.tsx
@@ -7,7 +7,7 @@ interface DropdownContentProps {
 
 export const DropdownContent: React.FC<DropdownContentProps> = ({children, open}) => {
     return (
-        <div className={`absolute left-0 top-full mt-2 w-[304px] bg-white rounded-[12px] border border-[#959494] shadow-md transition-all duration-200 ease-in-out transform 
+        <div className={`absolute left-0 top-full mt-2 w-[304px] bg-white rounded-[12px] border border-[#959494] shadow-md z-50 transition-all duration-200 ease-in-out transform 
         ${open ? "opacity-100 translate-y-0 visible" : "opacity-0 -translate-y-2 invisible"}`
         }>
         <ul className="py-2 space-y-[10px]">
